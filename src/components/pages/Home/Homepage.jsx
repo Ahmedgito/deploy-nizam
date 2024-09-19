f7798de3f65ca3c9485dbf35aaf5559177a52484
Ahmedgito <mamooabid1@gmail.com>
2024-09-19 18:19:10 +0500

    t

--- src/components/pages/Home/Homepage.jsx
+++ src/components/pages/Home/Homepage.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Layout from "../../layout/layout";
-import Section1 from "./Section1/Section1";
+import Layout from '../../layout/Layout';
+import Section1 from './Section1/Section1';
 import Section2 from './Section2/Section2'
-import Section3 from "./Section3/Section3";
+import Section3 from './Section3/Section3';
 
 
 
