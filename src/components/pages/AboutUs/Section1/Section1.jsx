28ed2b78a719999e729ed2f54d06e67e6e2cc508
Ahmedgito <mamooabid1@gmail.com>
2024-09-26 17:36:16 +0500

    Adjustments

--- src/components/pages/AboutUs/Section1/Section1.jsx
+++ src/components/pages/AboutUs/Section1/Section1.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import img1 from '../../../../assets/About/f-image.png' ;
+import img2 from '../../../../assets/About/A2.png' ;
+import img3 from '../../../../assets/About/A3.png' ;
 
 const Section1 = () =>{
     return(
@@ -44,11 +46,37 @@ const Section1 = () =>{
 
     {/* Right Column: Image */}
     <div className="flex justify-center md:justify-start items-center">
-      <img src={img1} alt="Introduction Image" className="max-w-full md:text-left text-center h-auto" />
+      <img src={img2} alt="Introduction Image" className="max-w-full md:text-left text-center h-auto" />
     </div>
   </div>
 </div>
 
+<div className="font-montserrat flex items-center m-20 px-4 md:px-20 bg-white">
+  <div className="grid grid-cols-1 md:grid-cols-2  md:gap-1 items-center">
+
+    {/* Left Column: Image */}
+    <div className="flex justify-center md:justify-end items-center">
+      <img src={img3} alt="Introduction Image" className="max-w-full md:text-left text-center h-auto " />
+    </div>
+
+    {/* Right Column: Text */}
+    <div className=" font-montserrat text-center px-2  md:text-left"> {/* Center text on mobile */}
+      <h2 className="text-xl  md:text-4xl font-[800] mb-4 text-[#4c301c]">
+      150 YEARS LATER</h2>
+      <p className="text-gray-600 text-sm md:text-lg  mb-6">
+      We are a diversified textile manufacturer, while our roots and expertise still is in Tents and Shelter, we have invested in new manufacturing facilities to become a diverse player in the global textile industry, servicing customers in over 100 Countries.
+      Our portfolio of products across our manufacturing facilities are:
+      </p>
+      <p className=" text-gray-600 text-sm md:text-lg">
+      -Tents and Shelter <br />
+      -Denim Jeans and Protective Clothing forWorkWear <br />
+      -Home Textile and Made Ups <br />
+      -Polyester Fleece Blanket <br />
+      </p>
+    </div>
+  </div>
+
+</div>
                 
         </>
     ) 
