f6fadd49363816f838a41c7b278a77411a9493a2
Ahmedgito <mamooabid1@gmail.com>
2024-10-02 14:32:00 +0500

    crousel fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,7 @@ import About from './components/pages/AboutUs/About';
 const App = () => {
   return(
     <>
-    
-
+  
       <Routes>
 
       <Route path="/" element={<Homepage />} />
--- src/components/pages/AboutUs/Section6/Section6.jsx
+++ src/components/pages/AboutUs/Section6/Section6.jsx
@@ -87,20 +87,19 @@ const Section6 = () => {
   ];
 
   return (
-    <div className="font-montserrat slider-container relative max-w-5xl mx-auto py-0 md:px-4 px-12">
+    <div className="font-montserrat slider-container relative max-w-5xl mx-auto py-0 md:px-4 px-12 mb-10">
       <Slider {...settings} className="flex items-center space-x-4">
         {slides.map((slide, index) => (
           <div
             key={index}
-            className="slide-item text-gray-600 transition-all duration-100 hover:text-white flex flex-col items-center align-middle justify-center md:py-20 hover:bg-orange-500 py-20 bg-gray-100 rounded-lg h-auto w-64 mx-auto group"
+            className="slide-item text-gray-600 transition-all duration-100 hover:text-white flex flex-col items-center justify-center md:py-20 hover:bg-orange-500 py-20 bg-gray-100 rounded-lg h-auto w-64 mx-auto group min-h-[300px]" // Added min-h to set a consistent height
           >
-         
             <img
               src={slide.icon}
               alt={`Slide ${index}`}
-              className="mb-4 mx-auto transition-all duration-300 group-hover:filter group-hover:brightness-0 group-hover:invert"
+              className="mb-4 w-32 h-32 object-contain mx-auto transition-all duration-300 group-hover:filter group-hover:brightness-0 group-hover:invert"
             />
-            <p className="text-center text-md italic font-normal">{slide.text}</p>
+            <p className="text-center md:text-md text-sm italic font-normal">{slide.text}</p>
           </div>
         ))}
       </Slider>
@@ -109,5 +108,6 @@ const Section6 = () => {
 };
 
 export default Section6;
+
 
 
