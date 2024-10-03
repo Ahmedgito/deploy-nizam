6c07b5fb34ba4f73ee493502d463d47c5930d171
Ahmedgito <mamooabid1@gmail.com>
2024-10-03 15:34:41 +0500

    fix

--- src/components/pages/Production/Production.jsx
+++ src/components/pages/Production/Production.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import Section1 from '../Production/Section1/Section'
 import Section2 from "./Section2/Section2";
 import Section3 from "./Section3/Section3";
-import Layout from "../../layout/layout";
+import Layout from "../../layout/Layout";
 
 const About = () => {
     useEffect(() => {
