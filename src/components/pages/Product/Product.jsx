b44383b54262f7dffe0f150c7a54dbe5f9aa58bf
Ahmedgito <mamooabid1@gmail.com>
2024-10-03 15:36:34 +0500

    fix

--- src/components/pages/Product/Product.jsx
+++ src/components/pages/Product/Product.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import Section1 from '../Product/Section1/Section1'
-import Layout from "../../layout/layout";
+import Layout from "../../layout/Layout.jsx";
 import Section2 from "./Section2/Section2";
 import Section3 from "./Section3/Section3";
 
