3920c370e1a251940fa19195ea3a6e1b2d6bd7fe
Ahmedgito <mamooabid1@gmail.com>
2024-10-07 12:23:40 +0500

    Mobile Section

--- src/components/pages/Sustainibility/Section2/Section2.jsx
+++ src/components/pages/Sustainibility/Section2/Section2.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useInView } from 'react-intersection-observer';
 import vestImage from '../../../../assets/Sustainibility/vest.png'
 import circel1 from '../../../../assets/Sustainibility/t_circle.png'
-
+import Sidecircle from '../../../../assets/Sustainibility/side_circle.png'
 
 const Section2 = React.forwardRef((props, sectionRef) => {
     // Intersection Observer hook for animations
@@ -49,23 +49,32 @@ const Section2 = React.forwardRef((props, sectionRef) => {
     </section>
     </section>
 
-   {/* Mobile-Only View */}
-<section className="sm:block md:hidden bg-white py-8">
-  <div className="max-w-full px-4 space-y-4 text-center">
+
+              {/* Mobile-Only View */}
+    <section className="sm:block md:hidden bg-white py-8">
+  <div className="max-w-full -mt-16 px-6 space-y-4 text-center">
+    <div>
+    <img src={circel1} alt="Nizam One Vest" className="w-full absolute top-64 h-full max-w-sm left-0 object-contain " />
+    </div>
     <h2 className="text-orange-500 font-bold text-2xl uppercase">
       Nizam One Initiative
     </h2>
-    <p className="text-gray-700 text-base leading-relaxed">
+
+    <div>
+    <img src={Sidecircle} alt="Nizam One Vest" className="w-24 z-10 opacity-85 absolute flex justify-start top-[55rem] h-64 max-w-sm left-0 object-contain " />
+    </div>
+
+    <p className="text-gray-700 text-lg leading-tight">
       Our secret to surviving 150 years is constantly raising the bar and innovating. Our prime focus going forward is sustainability, which is why we have launched our initiative Nizam One. It is our obsession to reimagine everything, making sustainability the number-one objective. Nizam One is a holistic approach trying to become one with the earth and people.
     </p>
-    <p className="text-gray-700 text-base leading-relaxed">
+    <p className="text-gray-700 text-lg leading-tight">
       Nizam One builds so our core values as an organization commit to the change. We are driving this initiative not only from our values but also picking up our process map of Input, Process, Output, and People.
     </p>
   </div>
 
   {/* Image Section */}
-  <div className="flex justify-center mt-8">
-    <img src={vestImage} alt="Nizam One Vest" className="w-48 h-auto object-contain" />
+  <div className="flex justify-end mt-8">
+    <img src={vestImage} alt="Nizam One Vest" className="w-56 h-auto   object-contain" />
   </div>
 </section>
 
