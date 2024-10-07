0af583bc4eecd7fb4741e283c28bd4dccea592e7
Ahmedgito <mamooabid1@gmail.com>
2024-10-07 17:15:43 +0500

    Fixed Minor Issues

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -95,7 +95,7 @@ const getBackgroundImage = () => {
         >
           <div className="flex justify-end p-4">
             <button onClick={toggleMenu} className="text-white focus:outline-none">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-9 h-9">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
               </svg>
             </button>
